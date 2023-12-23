76484eb67d2870c933ccbee584ab234dc296b76b
Alamin-22 <mdalaminmollick@gmail.com>
2023-12-24 00:51:30 +0600

    first commit

---
+++ src/Pages/HOme/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-red-400">
+                This is home components
+            </h1>
+        </div>
+    );
+};
+
+export default Home;
