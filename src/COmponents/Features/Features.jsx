47681287c78ddbbc6eba17483c7faec7b13ae9f6
Alamin-22 <mdalaminmollick@gmail.com>
2023-12-24 18:37:20 +0600

    section is upadted

--- src/COmponents/Features/Features.jsx
+++ src/COmponents/Features/Features.jsx
@@ -18,8 +18,8 @@ const Features = () => {
                         </p>
                     </div>
                     <div className="flex flex-wrap -m-4">
-                        <div className="xl:w-1/3 md:w-1/2 p-4">
-                            <div className="border border-gray-200 p-6 rounded-lg">
+                        <div className="xl:w-1/3 md:w-1/2 p-4 transform transition-transform hover:scale-105 cursor-pointer">
+                            <div className="border bg-red-100 border-gray-200 p-6 rounded-lg">
                                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                                     <FaCode className="text-2xl text-red-400 " />
                                 </div>
@@ -29,8 +29,8 @@ const Features = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="xl:w-1/3 md:w-1/2 p-4">
-                            <div className="border border-gray-200 p-6 rounded-lg">
+                        <div className="xl:w-1/3 md:w-1/2 p-4 transform transition-transform hover:scale-105 cursor-pointer">
+                            <div className="border bg-red-100 border-gray-200 p-6 rounded-lg">
                                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                                     <MdOutlineCorporateFare className="text-2xl text-red-400" />
                                 </div>
@@ -40,8 +40,8 @@ const Features = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="xl:w-1/3 md:w-1/2 p-4">
-                            <div className="border border-gray-200 p-6 rounded-lg">
+                        <div className="xl:w-1/3 md:w-1/2 p-4 transform transition-transform hover:scale-105 cursor-pointer">
+                            <div className="border bg-red-100 border-gray-200 p-6 rounded-lg">
                                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                                     <GiBank className="text-2xl text-red-400" />
                                 </div>
@@ -51,8 +51,8 @@ const Features = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="xl:w-1/3 md:w-1/2 p-4">
-                            <div className="border border-gray-200 p-6 rounded-lg">
+                        <div className="xl:w-1/3 md:w-1/2 p-4 transform transition-transform hover:scale-105 cursor-pointer" >
+                            <div className="border bg-red-100 border-gray-200 p-6 rounded-lg">
                                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                                     <SiMinds className="text-2xl text-red-600" />
                                 </div>
@@ -62,8 +62,8 @@ const Features = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="xl:w-1/3 md:w-1/2 p-4">
-                            <div className="border border-gray-200 p-6 rounded-lg">
+                        <div className="xl:w-1/3 md:w-1/2 p-4 transform transition-transform hover:scale-105 cursor-pointer">
+                            <div className="border bg-red-100 border-gray-200 p-6 rounded-lg">
                                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                                     <MdWork  className="text-2xl text-red-400" />
                                 </div>
@@ -73,8 +73,8 @@ const Features = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="xl:w-1/3 md:w-1/2 p-4">
-                            <div className="border border-gray-200 p-6 rounded-lg">
+                        <div className="xl:w-1/3 md:w-1/2 p-4 transform transition-transform hover:scale-105 cursor-pointer">
+                            <div className="border bg-red-100 border-gray-200 p-6 rounded-lg">
                                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                                     <FaPersonWalkingLuggage className="text-2xl text-red-400" />
                                 </div>
